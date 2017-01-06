c06cd36370ba672558a32c674aa94df795600c0d
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-01-06 18:58:41 +0100

    all badges

--- docs/badges/input/cli.py
+++ docs/badges/input/cli.py
@@ -46,7 +46,7 @@ for svg in svgs:
 #
 # DOCKER IMAGE SIZE
 #
-for number in list(range(12, 60)):
+for number in list(range(12, 99)):
     svgcode = render('svg-badge-template.jinja2', {
         'first':  { 'name': 'image', 'width': 50 },
         'second': { 'name': str(number) + ' MB', 'width': 44, 'color': '#627FFF' },
@@ -58,7 +58,7 @@ for number in list(range(12, 60)):
     print 'writing ' + docker_image_size_filename
     svgs.append({ 'filename': docker_image_size_filename })
 
-for number in list(range(190, 230)):
+for number in list(range(100, 350)):
     svgcode = render('svg-badge-template.jinja2', {
         'first':  { 'name': 'image', 'width': 50 },
         'second': { 'name': str(number) + ' MB', 'width': 55, 'color': '#627FFF' },
@@ -70,17 +70,6 @@ for number in list(range(190, 230)):
     print 'writing ' + docker_image_size_filename
     svgs.append({ 'filename': docker_image_size_filename })
 
-for number in list(range(320, 330)):
-    svgcode = render('svg-badge-template.jinja2', {
-        'first':  { 'name': 'image', 'width': 50 },
-        'second': { 'name': str(number) + ' MB', 'width': 55, 'color': '#627FFF' },
-    })
-    docker_image_size_filename = 'docker-image-size-' + str(number) + '.svg'
-    f = open('/pyapp/data/' + docker_image_size_filename , 'w+')
-    f.write(svgcode)
-    f.close()
-    print 'writing ' + docker_image_size_filename
-    svgs.append({ 'filename': docker_image_size_filename })
 
 
 #
