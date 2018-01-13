1939a700d0f7de6cfe1d441b53fa7ea18cf52ee6
clouless <github@gruenewaldt.net>
2018-01-13 13:02:18 +0100

    new badge

--- docs/badges/input/cli.py
+++ docs/badges/input/cli.py
@@ -51,6 +51,11 @@ svgs = [
         'second': { 'name': 'alpine 3.6', 'width': 65, 'color': '#627FFF' },
     },
     {
+        'filename': 'docker-from-alpine-3.7.svg',
+        'first':  { 'name': 'from', 'width': 40 },
+        'second': { 'name': 'alpine 3.7', 'width': 65, 'color': '#627FFF' },
+    },
+    {
         'filename': 'docker-from-ubuntu-16.04.svg',
         'first':  { 'name': 'from', 'width': 40 },
         'second': { 'name': 'ubuntu 16.04', 'width': 85, 'color': '#627FFF' },
